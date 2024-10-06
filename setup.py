2328acfdec30cea766d652e4d014d4010a382307
mp17 <palombomarco@yahoo.com>
2024-10-06 15:58:38 +0200

    aggiunta setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chromadb_modified",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Elenca qui le eventuali dipendenze di cui ha bisogno il tuo pacchetto
+    ],
+    author="IlTuoNome",
+    description="Versione modificata di chromadb",
+    url="https://github.com/mp17xk/chromadb_modified",
+)
